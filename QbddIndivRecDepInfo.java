a1207aaef9631c317b36c3b79046c9837496e88f
HarshaaTech <studiesharshaa@gmail.com>
2025-03-24 20:52:52 +0530

    Code for Extraction

---
+++ QbddIndivRecDepInfo.java
@@ -0,0 +1,14 @@
+
+1. Massachusetts Institute of Technology (MIT)
+2. Stanford University
+3. Carnegie Mellon University
+4. University of California, Berkeley
+5. Harvard University
+6. University of California, San Diego (UCSD)
+7. University of Washington
+8. California Institute of Technology (Caltech)
+9. University of Illinois Urbana-Champaign
+10. Georgia Institute of Technology
+11. University of Michigan, Ann Arbor
+12. Princeton University
+.
